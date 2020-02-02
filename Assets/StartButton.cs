9d1057617dd438936c1d166a0490f79c95d9c0b0
martyboggs <whack47@gmail.com>
2020-02-02 12:55:34 -0800

    title screen with start button

---
+++ Assets/StartButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartButton : MonoBehaviour
+{
+    public GameObject titleUi;
+
+    public void StartGame()
+    {
+        titleUi.SetActive(false);
+    }
+}
