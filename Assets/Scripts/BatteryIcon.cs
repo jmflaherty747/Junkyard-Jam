61d1f8d5b5e6afe76b7c8511be08708eb1b2a630
Shawn Dumdum <shawn.dumdum@yahoo.com>
2020-02-19 14:34:03 -0800

    added more levels

---
+++ Assets/Scripts/BatteryIcon.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BatteryIcon : MonoBehaviour
+{
+    private Camera mainCamera;
+    public Car car;
+    public Image icon;
+    public Vector3 offset;
+
+    void Awake()
+    {
+        mainCamera = Camera.main;
+        offset = transform.localPosition;
+    }
+
+    void Update()
+    {
+        icon.enabled = !car.battery;
+        var localToWorld = car.transform.TransformPoint(offset);
+        var worldToScreen = mainCamera.WorldToScreenPoint(localToWorld);
+        icon.transform.position = worldToScreen;
+
+    }
+}
